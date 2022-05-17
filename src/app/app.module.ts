bd567499b515b49f03f4fd33ee691bac884cc56d
mobreaux <105510837+mobreaux@users.noreply.github.com>
2022-05-16 21:30:01 -0500

    giphyAPI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { GiphyServiceComponent } from './giphy-service/giphy-service.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GiphyServiceComponent
   ],
   imports: [
     BrowserModule,
