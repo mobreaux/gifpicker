a37468bc0e81e2419d3dfaf7f76e8c4e132ec9fe
mobreaux <105510837+mobreaux@users.noreply.github.com>
2022-05-16 22:02:06 -0500

    UI updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { GiphyServiceComponent } from './giphy-service/giphy-service.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GiphyServiceComponent
+    GiphyServiceComponent,
+    HeaderComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
