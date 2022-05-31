5cc3ecc7d3fcd96952bed4c3f139d2ecb27bdc53
mobreaux <105510837+mobreaux@users.noreply.github.com>
2022-05-31 16:17:22 -0500

    Fixing error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { GiphyServiceComponent } from './giphy-service/giphy-service.component';
 import { HeaderComponent } from './header/header.component';
 import { ContentComponent } from './content/content.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
@@ -12,7 +11,6 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 @NgModule({
   declarations: [
     AppComponent,
-    GiphyServiceComponent,
     HeaderComponent,
     ContentComponent
   ],
